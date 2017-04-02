0dd51fd2611696ef65ffdcadd18068b08a8b92e4
boltean <olteanb@hotmail.com>
2017-04-02 13:40:34 -0700

    8

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,30 +22,52 @@ namespace ZenithCore.Controllers
 
 
 
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
             var firstDay = "";
             var lastDay = "";
+            string week = "this";
 
+            int index = 0;
+
+
+            if (id == 0)
+            {
+                index = 0;
+                week = "current";
+            }
+            else
+            {
+                index = 7 * id;
+                if (id < 0)
+                {
+                    week = "previous";
+                }
+                else
+                {
+                    week = "next";
+                }
+
+            }
 
 
             if ((int)System.DateTime.Now.DayOfWeek == 0)
             {
-                firstDay = DateTime.Now.AddDays(-6).ToString("MM/dd/yy") + " 12:00 AM";
+                firstDay = DateTime.Now.AddDays(index - 6).ToString("MM/dd/yy") + " 12:00 AM";
             }
             else
             {
-                firstDay = DateTime.Now.AddDays(1 - (int)System.DateTime.Now.DayOfWeek).ToString("MM/dd/yy") + " 12:00 AM";
+                firstDay = DateTime.Now.AddDays(index + 1 - (int)System.DateTime.Now.DayOfWeek).ToString("MM/dd/yy") + " 12:00 AM";
             }
 
 
             if ((int)System.DateTime.Now.DayOfWeek == 0)
             {
-                lastDay = System.DateTime.Now.ToString("MM/dd/yy") + " 23:59";
+                lastDay = System.DateTime.Now.AddDays(index).ToString("MM/dd/yy") + " 23:59";
             }
             else
             {
-                lastDay = (DateTime.Now.AddDays(7 - (int)System.DateTime.Now.DayOfWeek)).ToString("MM/dd/yy") + " 23:59";
+                lastDay = (DateTime.Now.AddDays(index + 7 - (int)System.DateTime.Now.DayOfWeek)).ToString("MM/dd/yy") + " 23:59";
             }
 
             DateTime maxDate = DateTime.Parse(lastDay);
@@ -53,12 +75,13 @@ namespace ZenithCore.Controllers
             ViewBag.DateUntil = maxDate;
             DateTime minDate = Convert.ToDateTime(firstDay);
 
-           // var events = _context.Events.Include(e => e.Activity);
+            // var events = _context.Events.Include(e => e.Activity);
 
             var events = from e in _context.Events.Include(a => a.Activity)
-                        where e.EventFrom >= minDate && e.EventTo <= maxDate && e.IsActive
+                         where e.EventFrom >= minDate && e.EventTo <= maxDate && e.IsActive
                          orderby e.EventFrom ascending
                          select e;
+            ViewBag.Week = week;
             return View(events.ToList());
         }
 
